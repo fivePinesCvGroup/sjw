c21a2233d25e862c65c38f24a477656da91b67fd
Shen <108865284+shenhandsome@users.noreply.github.com>
2022-07-14 10:56:24 +0800

    矿石代码
    目前根据矿石的图片，可以准确判断出具体的面，但在镜头情况下，因为资源有限，还未试过，但估计问题不大，我吸取了标识牌的灵感，筛选出面积第二大的轮廓，并将其用最小外接矩形框出，后由于空白面与其他面第二大面积误差很大，可以筛出空白面，再根据长宽比晒出R面和条形码面。

---
+++ kuangshi.cpp
@@ -0,0 +1,68 @@
+﻿#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+Mat img;
+int main()
+{
+	//VideoCapture cap(0);
+	//while (true) {
+		//cap.read(img);
+	    string path = "C:\\Users\\PC\\Desktop\\5.jpg";
+		img = imread(path);
+		Mat imgGray, imgBlur, imgHSV, imgCanny, imgTwo, imgDil, imgErode;
+		resize(img, img, Size(500, 500));
+		cvtColor(img, imgHSV, COLOR_BGR2HSV);
+		inRange(imgHSV, Scalar(18, 50, 103), Scalar(60, 255, 255), imgTwo);
+		Mat kernel = getStructuringElement(MORPH_RECT, Size(9, 9));
+		erode(imgTwo, imgErode, kernel);
+		dilate(imgErode, imgDil, kernel);
+		vector<vector<Point>>contours;
+		vector<Vec4i>hierarchy;
+		findContours(imgDil, contours, hierarchy, RETR_LIST, CHAIN_APPROX_NONE);
+		double maxarea = 0;
+		int m, t;
+		for (int i = 0; i < contours.size(); i++) {
+			double area = contourArea(contours[i]);
+			cout << area << endl;
+			if (area > maxarea) {
+				maxarea = area;
+				m = i;
+			}
+		}
+		maxarea = contourArea(contours[0], true);
+		for (int i = 0; i < contours.size() && i != m; i++) {
+			double area = contourArea(contours[i]);
+			if (area > maxarea) {
+				maxarea = area;
+				t = i;
+			}
+		}
+		RotatedRect rect = minAreaRect(contours[t]);
+		Point2f rectPoints[4];
+		rect.points(rectPoints);
+		for (int j = 0; j < 4; j++) {
+			line(img, rectPoints[j], rectPoints[(j + 1) % 4], Scalar(0, 255, 0), 4, 8, 0);
+		}
+		double a = rect.size.height / rect.size.width;
+		cout << a << endl;
+		double area1 = rect.size.height * rect.size.width;
+		cout << area1 << endl;
+		if (area1 < 20000) {
+			cout << "空白" << endl;
+		}
+		else if (a <=0.8) {
+		    cout << "R面" << endl;
+		}
+		else if (a > 0.8) {
+			cout << "条形码面" << endl;
+		}
+		imshow("img Two", imgTwo);
+		imshow("img Erode", imgErode);
+		imshow("img Dil", imgDil);
+		imshow("img", img);
+		waitKey(0);
+	return 0;
+}
